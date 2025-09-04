d6f3745e14e4be6f233f3aee8034255c16c6200c
JefersonDeLaCruz <dv23003@ues.edu.sv>
2025-09-04 12:56:53 -0600

    GGS

---
+++ app/ejercicios-php/ej2.php
@@ -0,0 +1,27 @@
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    
+</head>
+<body>
+    <form action="" method="post">
+        <label for="">Ingrese temp</label>
+        <input type="text">
+
+        <select name="" id="">
+            <option value="c_to_f">Celcius to Fa</option>
+            conver
+            <option value="f_to_c">Far to celcius</option>
+        </select>
+    </form>
+
+
+    <h2>im coocked</h2>
+    
+</body>
+</html>
