d5ce565b4dc57a654f777d18504706126ff41bdb
JefersonDeLaCruz <dv23003@ues.edu.sv>
2025-09-05 19:44:33 -0600

    ejercicio 2 done

--- app/ejercicios-php/ej2.php
+++ app/ejercicios-php/ej2.php
@@ -1,5 +1,32 @@
 
+<?php
+    // $mensajeError = "";
+    $temperatura = $_POST["temp"] ?? "";
+    $operaciones = ["Celsius a Fahrenheit", "Fahrenheit a Celsius"];
+    $operacion = $_POST["operacion"] ?? "";
+    if(!empty($temperatura) && !empty($operacion)){
+        if (is_numeric($temperatura)) {
+            # code...
+            if ($operacion == $operaciones[0]) {
+                $result = $temperatura * 1.8 + 32;
+                $mensaje = "{$temperatura} grados C equivalen a {$result} grados F";
+            }elseif($operacion == $operaciones[1]){
+                $result = ($temperatura - 32) / 1.8;
+                $mensaje = "{$temperatura} grados F equivalen a {$result} grados C";
+            }else{
+                $mensaje = "Error durante la conversion";
+            }
 
+        }else{
+            $mensaje = "Debe ingresar una temperatura valida";
+        }
+
+    }else{
+        $mensaje = "Debe seleccionar una operacion";
+    }
+
+
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -11,17 +38,19 @@
 <body>
     <form action="" method="post">
         <label for="">Ingrese temp</label>
-        <input type="text">
+        <input type="text" name="temp" value="">
 
-        <select name="" id="">
-            <option value="c_to_f">Celcius to Fa</option>
-            conver
-            <option value="f_to_c">Far to celcius</option>
+        <select name="operacion" id="">
+            <option value="<?= $operaciones[0]?>"><?= $operaciones[0]?></option>
+            <label for=""></label>
+            <option value="<?= $operaciones[1]?>"><?= $operaciones[1]?></option>
         </select>
+
+        <input type="submit" value="Convertir">
     </form>
 
 
-    <h2>im coocked</h2>
+    <h2><?= $mensaje ?? '' ?></h2>
     
 </body>
 </html>
